34243899ddfc4955ce46f2f7fd32df75016643b1
Jonathan Cacace <jonathan.cacace@gmail.com>
2021-02-15 22:03:58 +0100

    new parameters

--- src/force_plugin.cpp
+++ src/force_plugin.cpp
@@ -21,9 +21,11 @@ namespace gazebo
 
     // Pointer to the update event connection
     private: event::ConnectionPtr updateConnection;
-    private: physics::LinkPtr  _humm_base_link;
+    private: physics::LinkPtr  _base_link;
     private: geometry_msgs::Wrench _wrench;
     private: string _link_name;
+    private: int _body_frame_ref;
+    private: int _debug;
     private: string _ft_topic_name;
 
     public: void FTs_cb( geometry_msgs::Wrench w ) {
@@ -50,9 +52,11 @@ namespace gazebo
       _wrench.torque.x = 0.0;
       _wrench.torque.y = 0.0;
       _wrench.torque.z = 0.0;
-
-
-
+      
+      _debug = 0;
+      _body_frame_ref = 0;
+      _debug = _sdf->Get<int>("debug"); 
+      _body_frame_ref = _sdf->Get<int>("body_frame"); 
       _link_name = _sdf->Get<string>("link_name");
       _ft_topic_name = _sdf->Get<string>("topic_name");
       _ft_sub = _node_handle->subscribe(_ft_topic_name, 1, &ModelPush::FTs_cb, this );
@@ -61,23 +65,40 @@ namespace gazebo
     }
 
     // Called by the world update start event
-    public: void OnUpdate()
-    {   
+    public: void OnUpdate() {   
       common::Time currTime = this->model->GetWorld()->SimTime();
       // Apply a small linear velocity to the model.
-      if (currTime>=3) {
-        _humm_base_link = model->GetLink( _link_name );
+      //if (currTime>=3) {
+        _base_link = model->GetLink( _link_name );
+        if ( _base_link == NULL ) {
+          cout << "base link null!" << endl;
+        }
+        else { 
+          if ( _body_frame_ref == 1 ) {
+            _base_link->AddRelativeForce(ignition::math::Vector3d( _wrench.force.x, _wrench.force.y, _wrench.force.z ));
+            _base_link->AddRelativeTorque(ignition::math::Vector3d( _wrench.torque.x, _wrench.torque.y, _wrench.torque.z ));
+          }
+          else {
+            _base_link->AddForce(ignition::math::Vector3d( _wrench.force.x, _wrench.force.y, _wrench.force.z ));
+            _base_link->AddTorque(ignition::math::Vector3d( _wrench.torque.x, _wrench.torque.y, _wrench.torque.z ));
+          }
+        }	
+      //}
 
-        cout << "Publishing on: " << _ft_topic_name << endl;
-        cout << "Wrench: " << _wrench << endl;
-        _humm_base_link->AddRelativeForce(ignition::math::Vector3d( _wrench.force.x, _wrench.force.y, _wrench.force.z ));
-        _humm_base_link->AddRelativeTorque(ignition::math::Vector3d( _wrench.torque.x, _wrench.torque.y, _wrench.torque.z ));
-      }	
+
+      //if (_debug ) {
+      //  ignition::math::Vector3d applied_force =_base_link->RelativeForce(); //(ignition::math::Vector3d( _wrench.force.x, _wrench.force.y, _wrench.force.z ));
+      //  ignition::math::Vector3d applied_torque =_base_link->RelativeTorque(); //(ignition::math::Vector3d( _wrench.force.x, _wrench.force.y, _wrench.force.z ));
+      //  cout << "Desired force: " <<  _wrench.force.x << " " << _wrench.force.y << " " << _wrench.force.z << endl;
+      //  cout << "Applied force: " << applied_force[0] << " " << applied_force[1] << " " << applied_force[2]  << endl;
+      //  cout << "Desired torque: " <<  _wrench.torque.x << " " << _wrench.torque.y << " " << _wrench.torque.z << endl;
+      //  cout << "Applied torque: " << applied_torque[0] << " " << applied_torque[1] << " " << applied_torque[2]  << endl;
+      //}
 
 
     }
 
-
+    
   };
 
   // Register this plugin with the simulator
